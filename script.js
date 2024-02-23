6c8982c1e11057c7242526e4956fcb42d8805eee
Saim403 <155624749+Saim403@users.noreply.github.com>
2024-02-23 22:21:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let softwareCompany = {
+    name: "  Microsoft.",
+    founder: "Bill Gates ",
+    yearFounded: 2010,
+    products: ["Smartphones", "Cameras", "Led"]
+};
+
+console.log("Company Name:", softwareCompany.name);
+console.log("Founder:", softwareCompany.founder);
+console.log("Year Founded:", softwareCompany.yearFounded);
+console.log("Products:", softwareCompany.products);
